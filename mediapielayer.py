b217d860282f56c7cf432052b199dd2b2c19e41e
Mustafa Simav <ms@msimav.net>
2014-04-27 20:30:09 +0300

    base structure of web app

---
+++ mediapielayer.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def list_movies():
+    pass
+
+@app.route("/<movie>")
+def watch_movie(movie):
+    pass
+
+@app.route("/<movie>/subtitle")
+def find_subtitle(movie):
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
