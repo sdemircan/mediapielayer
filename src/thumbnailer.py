142b4f2acdfad319f1f006b9e56cecd357a04b85
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2014-04-28 01:02:21 +0300

    exception type specified at thumnail creator

--- src/thumbnailer.py
+++ src/thumbnailer.py
@@ -19,7 +19,7 @@ class Thumbnailer:
                                  str(size)
                                ])
             return thumbnail_path
-        except:
+        except subprocess.CalledProcessError:
             return None
 
 
