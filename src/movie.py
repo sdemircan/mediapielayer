0415ffd9d4c41a72629a2efb313983687ad06268
Mustafa Simav <ms@msimav.net>
2014-04-27 22:04:18 +0300

    classes to represent and list movies implemented

---
+++ src/movie.py
@@ -0,0 +1,24 @@
+import os
+import fnmatch
+
+class Movie:
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+
+    def path(self):
+        return os.path.join(self.path, self.name)
+
+    def pretty_name(self):
+        return self.name[:-4].replace('.', ' ').title()
+
+class MovieLister:
+    def __init__(self, basedir, extensions):
+        self.basedir = basedir
+        self.extensions = extensions
+
+    def list(self):
+        for root, dirs, files in os.walk(self.basedir):
+            for extension in self.extensions:
+                for filename in fnmatch.filter(files, extension):
+                    yield Movie(root, filename)
